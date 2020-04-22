e2389abc1cc9812e210c83d968431891a75aebd1
ksw5126 <ksw3149@naver.com>
2020-04-22 15:12:23 +0900

    #11 media, video

--- app/src/main/java/com/example/sns_project/WriteInfo.java
+++ app/src/main/java/com/example/sns_project/WriteInfo.java
@@ -1,15 +1,20 @@
 package com.example.sns_project;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class WriteInfo {
 
     private String title;
-    private String contents;
+    private ArrayList<String> contents;
     private String publisher;
+    private Date createdAt;
 
-    public WriteInfo(String name, String contents, String publisher) {
+    public WriteInfo(String name, ArrayList<String> contents, String publisher, Date createdAt) {
         this.title = name;
         this.contents = contents;
         this.publisher = publisher;
+        this.createdAt = createdAt;
     }
 
     public String getTitle() {
@@ -20,11 +25,11 @@ public class WriteInfo {
         this.title = title;
     }
 
-    public String getContents() {
+    public ArrayList<String> getContents() {
         return contents;
     }
 
-    public void setContents(String contents) {
+    public void setContents(ArrayList<String> contents) {
         this.contents = contents;
     }
 
@@ -34,5 +39,13 @@ public class WriteInfo {
 
     public void setPublisher(String publisher) {
         this.publisher = publisher;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
     }
 }
--- app/src/main/java/com/example/sns_project/activity/GalleryActivity.java
+++ app/src/main/java/com/example/sns_project/activity/GalleryActivity.java
@@ -1,6 +1,7 @@
 package com.example.sns_project.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -37,26 +38,30 @@ public class GalleryActivity extends BasicActivity {
         recyclerView.setAdapter(mAdapter);
     }
 
-    public static ArrayList<String> getImagesPath(Activity activity) {
+    public ArrayList<String> getImagesPath(Activity activity) {
         Uri uri;
         ArrayList<String> listOfAllImages = new ArrayList<String>();
         Cursor cursor;
-        int column_index_data, column_index_folder_name;
+        int column_index_data;
         String PathOfImage = null;
-        uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        Intent intent = getIntent();
+        String[] projection;
 
-        String[] projection = { MediaStore.MediaColumns.DATA,
-                MediaStore.Images.Media.BUCKET_DISPLAY_NAME };
+        if(intent.getStringExtra("media").equals("video")) {
+            uri = android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+            projection = new String[] { MediaStore.MediaColumns.DATA, MediaStore.Images.Media.BUCKET_DISPLAY_NAME };
+        } else {
+            uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            projection = new String[] { MediaStore.MediaColumns.DATA, MediaStore.Images.Media.BUCKET_DISPLAY_NAME };
+        }
 
-        cursor = activity.getContentResolver().query(uri, projection, null,
-                null, null);
+
+        cursor = activity.getContentResolver().query(uri, projection, null, null, null);
 
         column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
-        column_index_folder_name = cursor
-                .getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+
         while (cursor.moveToNext()) {
             PathOfImage = cursor.getString(column_index_data);
-
             listOfAllImages.add(PathOfImage);
         }
         return listOfAllImages;
